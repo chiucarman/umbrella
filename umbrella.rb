f5213b9bcaa737198e1243c623cfa300fa50c2a6
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-19 02:38:07 +0000

    fetched weather api key

--- umbrella.rb
+++ umbrella.rb
@@ -32,6 +32,7 @@ pp longitude = loc.fetch("lng")
 
 # I've already created a string variable above: pirate_weather_api_key
 # It contains sensitive credentials that hackers would love to steal so it is hidden for security reasons.
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 # Assemble the full URL string by adding the first part, the API token, and the last part together
 pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
